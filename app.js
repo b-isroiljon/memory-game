f5db3dea34702d3b1e020368322eec85e0ef4933
Boruev Isroiljon <140053898+b-isroiljon@users.noreply.github.com>
2024-01-29 04:18:57 +0600

    update js code

--- app.js
+++ app.js
@@ -5,45 +5,74 @@ const praise = document.querySelector(".congratulations");
 const wrongAnswer = document.getElementById("wrongAudio");
 const correctAnswer = document.getElementById("correctAudio");
 
-let numbers = [];
+let rows = 1;
+const cols = 4;
+const uniqs = {};
 let opened = null;
 let score = 0;
 let lives = 3;
 let bestScore = 0;
 let delay = 1000;
+let currentLevel = 0;
 
-document.addEventListener("DOMContentLoaded", () =>
-  setTimeout(() => {
-    bestScore = localStorage.getItem("bestScore") || 0;
-    updateDisplay();
-    gameInit();
-  }, delay)
-);
-
-const newButtons = () => {};
-
-const resetRandomtSet = () => {
-  numbers = [1, 1, 2, 2];
+const generateNums = (rows, cols) => {
+  const numOfUniqs = rows * 2;
+  const nums = [];
+  for (let i = 0; i < rows; i++) {
+    let n = [];
+    while (n.length < cols) {
+      let num = Math.ceil(Math.random() * numOfUniqs);
+      if (uniqs[num]) uniqs[num]++;
+      else uniqs[num] = 1;
+      if (uniqs[num] > 2) continue;
+      n.push(num);
+    }
+    nums.push(n);
+  }
+  return nums;
 };
 
-// Функция для обновления цифр на кнопках
-const randomButtons = () => {
-  if (numbers.length < 2) resetRandomtSet();
-  buttons.forEach((button) => {
-    const randomIndex = Math.floor(Math.random() * numbers.length);
-    button.textContent = numbers[randomIndex];
-    numbers.splice(randomIndex, 1);
+const createButton = (nums) =>
+  nums.map((row) =>
+    row.map((num) => {
+      const btn = document.createElement("button");
+      btn.className = "btn";
+      btn.innerHTML = num;
+      return btn;
+    })
+  );
+
+const nums = generateNums(rows, cols);
+const buttonArray = createButton(nums);
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    bestScore = localStorage.getItem("bestScore") || 0;
+    createButtonsFromHtmlArray(buttonArray);
+    updateDisplay();
+    gameInit();
+  }, delay);
+});
+
+const createButtonsFromHtmlArray = (buttonArray) => {
+  const container = document.querySelector(".grid-container");
+  container.innerHTML = "";
+  buttonArray.forEach((row) => {
+    row.forEach((button) => {
+      container.appendChild(button);
+      setTimeout(() => {
+        button.classList.toggle("hidden", true);
+        button.addEventListener("click", openButton);
+      }, delay);
+    });
   });
-  resetRandomtSet();
 };
 
 const gameInit = () => {
-  setTimeout(() => {
-    buttons.forEach((button) => {
-      button.classList.toggle("hidden", true);
-      button.addEventListener("click", openButton);
-    });
-  }, delay);
+  buttons.forEach((button) => {
+    button.classList.toggle("hidden", true);
+    button.addEventListener("click", openButton);
+  });
 };
 
 const openButton = (event) => {
@@ -80,9 +109,9 @@ const handleSuccess = (button) => {
   opened.classList.toggle("success", true);
   button.removeEventListener("click", openButton);
   opened.removeEventListener("click", openButton);
+  correctAnswer.play();
   opened = null;
   score++;
-  correctAnswer.play();
 };
 
 const handleWrongButtons = (button) => {
@@ -133,33 +162,35 @@ const showGameOverModal = () => {
   });
 };
 
+const checkAllButtons = () => {
+  const buttonsArray = Array.from(
+    document.querySelectorAll(".grid-container .btn")
+  );
+
+  const allButtonsShownSuccess = buttonsArray.every((button) =>
+    button.classList.contains("success")
+  );
+  if (allButtonsShownSuccess) {
+    rows++;
+    praise.classList.add("praise");
+    setTimeout(() => {
+      buttonsArray.forEach((btn) => {
+        btn.classList.remove("success", "wrong", "shown");
+        setTimeout(() => {
+          btn.classList.toggle("hidden", true);
+          btn.addEventListener("click", openButton);
+        }, delay);
+      });
+      praise.classList.remove("praise");
+
+      createButtonsFromHtmlArray(buttonArray);
+      console.log(rows);
+    }, 1000);
+  }
+};
+
 const updateDisplay = () => {
   document.querySelector("#score").textContent = score;
   document.querySelector("#lives").innerHTML = "❤".repeat(lives);
   document.querySelector("#best-score").textContent = bestScore;
-};
-
-const checkAllButtons = () => {
-  const allButtonsShownSuccess = buttons.every((button) =>
-    button.classList.contains("success")
-  );
-  if (allButtonsShownSuccess) {
-    praise.classList.add("praise");
-    setTimeout(() => {
-      continueWithScore();
-      praise.classList.remove("praise");
-    }, 1000);
-  }
-  console.log(allButtonsShownSuccess);
-};
-
-const continueWithScore = () => {
-  setTimeout(() => {
-    buttons.forEach((btn) => {
-      btn.classList.remove("success", "wrong");
-      btn.classList.toggle("shown", false);
-    }, 600);
-  });
-  gameInit();
-  randomButtons();
 };
---
+++ index.js
@@ -0,0 +1,164 @@
+const wrongAnswer = document.getElementById("wrongAudio");
+const correctAnswer = document.getElementById("correctAudio");
+
+let rows = 1;
+let cols = 4;
+let delay = 1000;
+let score = 0;
+let opened = null;
+let = bestScore = 0;
+let lives = 3;
+
+// TODO: need to check correctness
+const generateNums = (rows, cols) => {
+  const uniqs = {};
+  const numOfUniqs = rows * 2;
+  const nums = [];
+  for (let i = 0; i < rows; i++) {
+    let n = [];
+    while (n.length < cols) {
+      let num = Math.ceil(Math.random() * numOfUniqs);
+      if (uniqs[num]) uniqs[num]++;
+      else uniqs[num] = 1;
+      if (uniqs[num] > 2) continue;
+      n.push(num);
+    }
+    nums.push(n);
+  }
+  return nums;
+};
+
+const createButton = (nums) =>
+  nums.map((row) =>
+    row.map((num) => {
+      const btn = document.createElement("button");
+      btn.className = "btn";
+      btn.innerHTML = num;
+      return btn;
+    })
+  );
+
+const createButtonsFromHtmlArray = (buttonArray) => {
+  const container = document.querySelector(".grid-container");
+  container.innerHTML = "";
+  buttonArray.forEach((row) => {
+    row.forEach((button) => {
+      container.appendChild(button);
+      setTimeout(() => {
+        button.classList.toggle("hidden", true);
+        button.addEventListener("click", handleOpenButton);
+      }, delay);
+    });
+  });
+};
+
+const updateDisplay = () => {
+  document.querySelector("#score").textContent = score;
+  document.querySelector("#lives").innerHTML = "❤".repeat(lives);
+  document.querySelector("#best-score").textContent = bestScore;
+};
+
+const resetButtons = (button) => {
+  // close first
+  opened.classList.toggle("hidden", true);
+  opened.classList.toggle("shown", false);
+  opened.classList.toggle("wrong");
+  // close second
+  button.classList.toggle("hidden", true);
+  button.classList.toggle("shown", false);
+  button.classList.toggle("wrong");
+  opened = null;
+};
+
+const showGameOverModal = () => {
+  const modal = document.getElementById("modal");
+  const restartButton = document.querySelector(".restart");
+  modal.style.display = "flex";
+  restartButton.addEventListener("click", () => {
+    score = 0;
+    lives = 3;
+    rows = 1;
+    gameInit();
+    modal.style.display = "none";
+  });
+};
+
+const checkTotalScore = () => {
+  localStorage.setItem("bestScore", bestScore);
+  if (lives === 0) setTimeout(showGameOverModal, 400);
+};
+
+const handleWrongButtons = (button) => {
+  button.classList.toggle("wrong", true);
+  opened.classList.toggle("wrong", true);
+  lives--;
+  setTimeout(() => resetButtons(button), 450);
+  wrongAnswer.play();
+  checkTotalScore();
+};
+
+const showCongrats = () => {
+  const praise = document.querySelector(".congratulations");
+  praise.classList.add("praise");
+  setTimeout(() => praise.classList.remove("praise"), delay);
+};
+
+const checkIfAllCorrect = () => {
+  const buttons = document.querySelectorAll(".grid-container .btn");
+  const isAllcorrect = Array.from(buttons).every((btn) =>
+    btn.classList.contains("success")
+  );
+
+  if (!isAllcorrect) return;
+  if (rows < 4) rows++;
+  showCongrats();
+  gameInit();
+};
+
+const handleSuccess = (button) => {
+  button.classList.toggle("success", true);
+  opened.classList.toggle("success", true);
+  button.removeEventListener("click", handleOpenButton);
+  opened.removeEventListener("click", handleOpenButton);
+  score++;
+  opened = null;
+  if (score > bestScore) bestScore = score;
+  checkIfAllCorrect();
+  correctAnswer.play();
+};
+
+const secondButton = (button) => {
+  const firstButtonNumber = parseInt(opened.textContent);
+  const secondButtonNumber = parseInt(button.textContent);
+  if (firstButtonNumber === secondButtonNumber) {
+    handleSuccess(button);
+  } else {
+    handleWrongButtons(button);
+  }
+  updateDisplay();
+};
+
+const handleOpenButton = (event) => {
+  const button = event.target;
+  if (button.classList.contains("shown")) return;
+  button.classList.toggle("shown", true);
+  button.classList.toggle("hidden", false);
+
+  if (!opened) {
+    opened = button;
+  } else {
+    secondButton(button);
+  }
+};
+
+const gameInit = () => {
+  setTimeout(() => {
+    bestScore = localStorage.getItem("bestScore") || 0;
+    const nums = generateNums(rows, cols);
+    const buttons = createButton(nums);
+    createButtonsFromHtmlArray(buttons);
+    updateDisplay();
+  }, delay);
+};
+
+document.addEventListener("DOMContentLoaded", gameInit);
